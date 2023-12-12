2ae280b24833214e8c5d88f5c368aecc37f07e64
Mahbubul Alam <mahbublalam500@gmail.com>
2023-12-13 00:08:58 +0600

    article section [Home page]

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Customers from "./components/home/customers";
 import About from "./components/home/about";
 import Members from "./components/home/members";
 import Packages from "./components/home/packages";
+import News from './components/home/News';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <About />
       <Members />
       <Packages />
+      <News />
     </>
   );
 }
