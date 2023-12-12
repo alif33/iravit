0acb91ba618154c55c522809000a7a72277fcb9a
Mahbubul Alam <mahbublalam500@gmail.com>
2023-12-13 01:08:56 +0600

    layout update

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,19 @@
-import Image from 'next/image'
-import Navbar from "./components/base/navbar";
 import Hero from "./components/home/hero";
 import Customers from "./components/home/customers";
 import About from "./components/home/about";
 import Members from "./components/home/members";
 import Packages from "./components/home/packages";
-import News from './components/home/News';
-import Footer from "./components/home/footer";
+import News from "./components/home/News";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Customers />
       <About />
       <Members />
       <Packages />
       <News />
-      <Footer />
     </>
   );
 }
