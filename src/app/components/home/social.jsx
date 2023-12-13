da1d772be5c44c6e4550b561a353ad9a8ba91a3c
promahbubul <mahbublalam500@gmail.com>
2023-12-14 01:44:57 +0600

    social section [Home page]

--- src/app/components/home/social.jsx
+++ src/app/components/home/social.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Container from '../ui/Container'
 import Image from 'next/image'
+import { FaArrowRight } from "react-icons/fa6";
+
 import socialImage1 from '../../../assets/social/social (1).png'
 import socialImage2 from '../../../assets/social/social (2).png'
 import socialImage3 from '../../../assets/social/social (3).png'
@@ -8,23 +10,29 @@ import Button from '../ui/Button'
 
 const Social = () => {
   return (
-    <div>
+    <div className='py-32 my-20 bg-grey-950'>
       <Container>
-       <div className="flex flex-row">
+        <div className="flex flex-row gap-14 items-center justify-between ">
           {/* Social Left */}
-          <div className="grid grid-cols-5 gap-5 ">
-            <Image className='col-span-2' src={socialImage1} />
-            <Image className='col-span-3' src={socialImage2} />
-            <Image className='col-span-5' src={socialImage3} />
+          <div className="flex flex-col w-full flex-1 gap-6">
+            <div className="flex flex-row flex-1 w-full gap-6 overflow-hidden">
+              <Image className='col-span-2  w-auto' src={socialImage1} />
+              <Image className='col-span-3  pt-16 w-full' src={socialImage2} />
+              </div>
+           <div className="flex-1">
+             <Image className='col-span-5  w-full' src={socialImage3} />
+           </div>
           </div>
           {/* Social Right */}
-          <div className="">
-            <h2 className=""></h2>
-            <Button text="See Pricings" btnPadding={"py-3 px-6"} >
-              
+          <div className=" flex-1">
+            <h2 className="font-poppins font-semibold text-h2 text-white leading-[60px] mb-12">Helping you overcome your any kind of technology challenges</h2>
+           <div className="flex flex-row">
+             <Button text="See Pricings" bgColor={"bg-brand-1"} color="text-white" btnPadding={"py-3 px-14"} >
+               < FaArrowRight />
             </Button>
+           </div>
           </div>
-       </div>
+        </div>
       </Container>
     </div>
   )
--- src/app/components/ui/Button.js
+++ src/app/components/ui/Button.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Button = ({ text, bgColor, color, btnPadding }) => {
+const Button = ({ text, bgColor, color, btnPadding, children }) => {
   return (
     <button
-      className={`${btnPadding}  font-medium ${color} text-sm ${bgColor} rounded-md`}
+      className={`${btnPadding} ${children && "flex flex-row gap-2 items-center"}  font-medium ${color} text-sm ${bgColor} rounded-md`}
     >
-      {text}
+      {text} {children}
     </button>
   );
 };
