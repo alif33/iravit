bc438eddec86ba8ba28cc21d3dc05f38797cdb29
promahbubul <mahbublalam500@gmail.com>
2023-12-14 01:14:58 +0600

    social section update

--- src/app/components/home/social.jsx
+++ src/app/components/home/social.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Container from '../ui/Container'
+import Image from 'next/image'
+import socialImage1 from '../../../assets/social/social (1).png'
+import socialImage2 from '../../../assets/social/social (2).png'
+import socialImage3 from '../../../assets/social/social (3).png'
+import Button from '../ui/Button'
 
 const Social = () => {
   return (
-    <div>social</div>
+    <div>
+      <Container>
+       <div className="flex flex-row">
+          {/* Social Left */}
+          <div className="grid grid-cols-5 gap-5 ">
+            <Image className='col-span-2' src={socialImage1} />
+            <Image className='col-span-3' src={socialImage2} />
+            <Image className='col-span-5' src={socialImage3} />
+          </div>
+          {/* Social Right */}
+          <div className="">
+            <h2 className=""></h2>
+            <Button text="See Pricings" btnPadding={"py-3 px-6"} >
+              
+            </Button>
+          </div>
+       </div>
+      </Container>
+    </div>
   )
 }
 
