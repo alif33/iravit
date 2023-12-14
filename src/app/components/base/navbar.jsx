0e705ca92c150a5055748949e6484b27eaf7d5b0
promahbubul <mahbublalam500@gmail.com>
2023-12-14 12:23:47 +0600

    navbar update

--- src/app/components/base/navbar.jsx
+++ src/app/components/base/navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import AllLinks from "./AllLinks";
 import logo from "../../../assets/logo.png";
 import Container from "../ui/Container";
@@ -8,63 +8,17 @@ import Container from "../ui/Container";
 const Navbar = ({ children }) => {
   return (
     <Container>
-      <div className="drawer">
-        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col">
-          {/* Navbar */}
-          <div className="w-full navbar p-0">
-            <div className="flex-none lg:hidden">
-              <label
-                htmlFor="my-drawer-3"
-                aria-label="open sidebar"
-                className="btn btn-square btn-ghost"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-6 h-6 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </label>
-            </div>
-            <div className="flex-1">
-              <Link href="/">
-                <Image src={logo} width={100} height={20} />
-              </Link>
-            </div>
-            <div className="flex-none hidden lg:block">
-              <ul className="menu gap-5 menu-horizontal text-lg text-[04091E] font-medium font-urbanist items-center">
-                {/* Navbar menu content here */}
-                <AllLinks />
-              </ul>
-            </div>
+      <div className="flex flex-row justify-between w-full  items-center py-2 ">
+        {/* Navbar Left */}
+        <div className="flex-1">
+          <Link href="/">
+            <Image src={logo} width={100} height={20} />
+          </Link>
+        </div>
+        {/* Navbar Right */}
+          <div className="flex-1 gap-5 flex flex-row items-center justify-end">
+        <AllLinks />
           </div>
-          {/* Page content here */}
-          {children}
-        </div>
-        <div className="drawer-side">
-          <label
-            htmlFor="my-drawer-3"
-            aria-label="close sidebar"
-            className="drawer-overlay"
-          ></label>
-          <ul className="menu p-4 w-80 min-h-full bg-base-200">
-            {/* Sidebar content here */}
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
-          </ul>
-        </div>
       </div>
     </Container>
   );
