78801f8a93ebd83e8c74de94ad8a07b324f2b7fa
Mahbubul Alam <mahbublalam500@gmail.com>
2023-12-14 16:19:31 +0600

    Price page

--- src/app/price/page.js
+++ src/app/price/page.js
@@ -1,15 +1,8 @@
+import Packages from "../components/home/packages";
 import Container from "../components/ui/Container";
 
 const PricingPage = () => {
-  return (
-    <div className="h-[28.5vh] flex items-center justify-center">
-      <Container>
-        <h1 className="text-5xl text-center border-b-8 border-brand-2 text-brand-1 font-extrabold py-5">
-          Price Page
-        </h1>
-      </Container>
-    </div>
-  );
+  return <Packages />;
 };
 
 export default PricingPage;
