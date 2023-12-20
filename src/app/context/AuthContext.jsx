f1e38ba801b48faf42d3297cb7040b04f3343b62
Mahbubul Alam <mahbublalam500@gmail.com>
2023-12-21 00:52:34 +0600

    update cursor

--- src/app/context/AuthContext.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-const createContext = useState(null);
-
-const ScrollContext = () => {
-  return <createContext.>ScrollContext</div>;
-};
-
-export default ScrollContext;
